9c276d9a98691688249bf61e3cf5ccb665f3b33b
RFHold <Robert@Robhold.com>
2019-06-19 14:45:30 -0400

    export forms

---
+++ src/components/Forms/index.js
@@ -0,0 +1,8 @@
+import ClientForm from './Client';
+import CompanyForm from './Company';
+import UserForm from './User';
+import ProjectForm from './Project';
+import TaskForm from './Task';
+import LoginForm from './Login';
+
+export { ClientForm, CompanyForm, UserForm, ProjectForm, TaskForm, LoginForm }
