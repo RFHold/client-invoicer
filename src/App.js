5623df766df31ad2e4927f234082fecfbee59801
kthor93 <teahorsen@gmail.com>
2019-06-19 12:06:46 -0400

    more components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import GlobalHeader from "./components/GlobalHeader";
 import Navbar from "./components/Navbar";
+import PageContainer from "./components/PageContainer";
 
 class App extends React.Component {
   render() {
@@ -9,7 +12,16 @@ class App extends React.Component {
       <Router>
         <div>
           <GlobalHeader />
-          <Navbar />
+          <Grid fluid id="app-container">
+            <Row id="app-row">
+              <Col xs={3} id="navbar-col">
+                <Navbar />
+              </Col>
+              <Col xs={9} id="pagecontainer-col">
+                <PageContainer />
+              </Col>
+            </Row>
+          </Grid>
         </div>
       </Router>
     );
--- src/components/PageContainer/index.js
+++ src/components/PageContainer/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { Link } from "react-router-dom";
+import "./style.css";
 
 function PageContainer() {
-	return null;
+	return (
+		<Grid fluid id="page-container">
+			<Row>
+				<Col xs={12}>
+					<p>Hello World</p>
+				</Col>
+			</Row>
+		</Grid>
+	);
 }
 
 export default PageContainer;
