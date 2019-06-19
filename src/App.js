64ddcab78b1bfc9f53edb6007466bc9ccb5a212c
RFHold <Robert@Robhold.com>
2019-06-19 14:45:18 -0400

    app to pure component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {PureComponent as Component} from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import Form from './components/Form';
 
-class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      company: 1
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  componentDidUpdate() {
+  }
+
+  componentWillUnmount() {
+  }
+
   render() {
     return (
       <Grid fluid>
         <Row>
           <Col xs={12}>
             Hello, world!
-            <Form method="POST" action="/api/session">
-              <h1>Form</h1>
-              <div>
-                <label htmlFor="sessionFormLoginInput">Username or Email</label>
-                <input id="sessionFormLoginInput" name="login" type="text" />
-              </div>
-              <div>
-                <label htmlFor="sessionFormPasswordInput">Password</label>
-                <input id="sessionFormPasswordInput" name="password" type="password" />
-              </div>
-              <button type="submit">Login</button>
-            </Form>
           </Col>
         </Row>
       </Grid>
