f5925372ac79e6017a36b8391b0fa6a256197a67
Robert Holden <robert@robhold.com>
2019-06-20 11:38:28 -0400

    routes work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, {PureComponent as Component} from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import GlobalHeader from "./components/GlobalHeader";
 import Navbar from "./components/Navbar";
 import PageContainer from "./components/PageContainer";
-import Dashboard from "./components/PageContainer/Dashboard";
-import Projects from "./components/PageContainer/Dashboard";
-import Clients from "./components/PageContainer/Dashboard";
-import Invoices from "./components/PageContainer/Dashboard";
-import Reports from "./components/PageContainer/Dashboard";
 import { CompanyContext } from "./components/Contexts";
 
 class App extends Component {
@@ -58,15 +53,7 @@ class App extends Component {
                   <Navbar />
                 </Col>
                 <Col xs={10} id="pagecontainer-col">
-                  <PageContainer>
-                    <Switch>
-                      <Route exact path="/dashboard" component={Dashboard} />
-                      <Route exact path="/projects" component={Projects} />
-                      <Route exact path="/clients" component={Clients} />
-                      <Route exact path="/invoices" component={Invoices} />
-                      <Route exact path="/reports" component={Reports} />
-                    </Switch>
-                  </PageContainer>
+                  <PageContainer/>
                 </Col>
               </Row>
             </Grid>
--- src/components/PageContainer/index.js
+++ src/components/PageContainer/index.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Projects from "./Projects";
+import Clients from "./Clients";
+import Invoices from "./Invoices";
+import Reports from "./Reports";
 import "./style.css";
 
 function PageContainer(props) {
-	return props.children;
+	return (
+		<Switch>
+			<Route exact path="/dashboard" component={Dashboard} />
+			<Route exact path="/projects" component={Projects} />
+			<Route exact path="/clients" component={Clients} />
+			<Route exact path="/invoices" component={Invoices} />
+			<Route exact path="/reports" component={Reports} />
+		</Switch>
+	);
 }
 
 export default PageContainer;
