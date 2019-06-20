6703c94472a35c168106f1e8dfa20b7f815bfc8b
mdh206 <49321381+mdharris206@users.noreply.github.com>
2019-06-20 13:58:17 -0400

    basic listview for clients

---
+++ src/components/ListView/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+
+class ListView extends React.PureComponent {
+  state = {
+    results: []
+  };
+
+  componentDidMount() {
+    axios({
+      method: "GET",
+      url: this.props.resource
+    })
+      .then(response => {
+        this.setState({ results: response.data.results || [response.data.result]});
+      })
+      .catch(error => {
+        this.onError(error);
+      });
+  }
+
+  render() {
+    const ItemComponent = this.props.itemComponent;
+    return this.state.results.map(item => <ItemComponent key={item.id} data={item} />);
+  }
+}
+
+export default ListView;
--- src/components/PageContainer/Clients/index.js
+++ src/components/PageContainer/Clients/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 // import "./style.css";
+import ListView from '../../ListView';
+
+function ListItem ({ data: client }) {
+    return (
+        <div>
+            <h1>{client.name}</h1>
+        </div>
+    );
+}
 
 function Clients() {
+
     return (
         <Grid fluid id="clients-container">
             <Row>
                 <Col xs={12}>
                     <p>Clients says hello</p>
+                    <ListView itemComponent={ListItem} resource="/api/company/1/clients"/>
                 </Col>
             </Row>
         </Grid>
