627419232f30f68aef129b387ba94ca8b3268ecc
Robert Holden <robert@robhold.com>
2019-06-20 11:25:49 -0400

    setting up browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {PureComponent as Component} from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import GlobalHeader from "./components/GlobalHeader";
@@ -32,26 +32,28 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div>
-        <GlobalHeader />
-        <Grid fluid id="app-container">
-          <Row id="app-row">
-            <Col xs={2} id="navbar-col">
-              <Navbar>
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route exact path="/projects" component={Projects} />
-                <Route exact path="/clients" component={Clients} />
-                <Route exact path="/invoices" component={Invoices} />
-                <Route exact path="/reports" component={Reports} />
-              </Navbar>
-            </Col>
-            <Col xs={10} id="pagecontainer-col">
-              <PageContainer />
-            </Col>
-          </Row>
-        </Grid>
-      </div>
-    </Router>
+        <div>
+          <GlobalHeader />
+          <Grid fluid id="app-container">
+            <Row id="app-row">
+              <Col xs={2} id="navbar-col">
+                <Navbar/>
+              </Col>
+              <Col xs={10} id="pagecontainer-col">
+                <PageContainer>
+                    <Switch>
+                      <Route exact path="/dashboard" component={Dashboard} />
+                      <Route exact path="/projects" component={Projects} />
+                      <Route exact path="/clients" component={Clients} />
+                      <Route exact path="/invoices" component={Invoices} />
+                      <Route exact path="/reports" component={Reports} />
+                    </Switch>
+                </PageContainer>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/PageContainer/index.js
+++ src/components/PageContainer/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-import { Link } from "react-router-dom";
 import "./style.css";
 
-function PageContainer() {
-	return null;
+function PageContainer(props) {
+	return props.children;
 }
 
 export default PageContainer;
