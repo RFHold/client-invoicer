b05602ed6f780e0506ece67d47bfcf757135b698
mdh206 <49321381+mdharris206@users.noreply.github.com>
2019-06-26 13:59:57 -0400

    dashboard updates

--- src/components/Private Views/Pages/Dashboard/index.js
+++ src/components/Private Views/Pages/Dashboard/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 //import "./style.css";
-import { TimeCard, TaskCard, ProjectCard } from "../../Cards";
+import {ProjectCard, TimeCard, TaskCard} from "../../../Private Views/Cards"
 
 function Dashboard() {
     return (
         <Grid fluid id="dashboard-container">
-            <Row>
-                <Col xs={12}>
-                    <h1>Dashboard</h1>
-                </Col>
-            </Row>
+            <h1>Dashboard</h1>
             <Row>
                 <Col xs={12} lg={4}>
-                    <TimeCard />
-                </Col>
+                <p>Current Projects</p>
+                    <ProjectCard />
+                </Col>  
                 <Col xs={12} lg={4}>
+                <p>Open Tasks</p>
                     <TaskCard />
                 </Col>
                 <Col xs={12} lg={4}>
-                    <ProjectCard />
-                </Col>
+                <p>Time Cards</p>
+                    <TimeCard />
+                </Col> 
             </Row>
         </Grid>
     );
