b916adfeafc131a313fb687a21f5535681c77d1d
Robert Holden <robert@robhold.com>
2019-06-28 14:31:17 -0400

    fixed build and prod deployment

--- app.js
+++ app.js
@@ -32,8 +32,12 @@ app.use(session({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.use(express.static(__root + "/build"));
 app.use(require(__root + "/router/router"))
+app.get("*", (req, res) => {
+    res.sendFile(__root + "/build/index.html")
+});
 
 app.listen(PORT, function() {
     console.log("App listening to port: " + PORT);
